c13c8811f4cb8e898a21ef5841eafb5b7d6f6132
ehrbs0327 <ehrbs0327@gmail.com>
2024-11-18 15:38:41 +0900

    [FEAT] 타워 업그레이드
    에센스 업그레이드 / 일반 업그레이드 구분
    에센스 업그레이드 -> Material변경 & 피격 시 효과 변경
    일반 업그레이드 -> 스텟 증가
    
    피격 시 효과(에센스 업글)
    - Sand : 슬로우
    - Fire : 화상
    - Wood : 일반 업글에서 내구도 수치 올릴 예정
    - Snow : 확률 얼리기 -> 박서진씨가 해결 예정

---
+++ 25212120_proj1/Assets/kdk/KDG_Stat_Scripts/LivingEntities/KDG_Structures/TestTower.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTower : BaseStructure
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    public override void Attack(BaseMonster target)
+    {
+        base.Attack(target);
+    }
+}

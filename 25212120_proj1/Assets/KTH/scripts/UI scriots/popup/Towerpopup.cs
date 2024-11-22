04ce9068c473e94890d5c6017879c39a899aefa3
SHI0284 <gpdls0411@naver.com>
2024-11-22 16:57:59 +0900

    UI

--- 25212120_proj1/Assets/KTH/scripts/UI scriots/popup/Towerpopup.cs
+++ 25212120_proj1/Assets/KTH/scripts/UI scriots/popup/Towerpopup.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Towerpopup : MonoBehaviour
 {
@@ -14,13 +13,48 @@ public class Towerpopup : MonoBehaviour
         }
     }
 
-    void OnMouseDown()
+    void Update()
     {
-        if (CompareTag("tower"))  // 특정 태그 확인
+        // 마우스 왼쪽 버튼 클릭 감지
+        if (Input.GetMouseButtonDown(0))
         {
-            if (popupPanel != null)
+            // 만약 UI 요소 위를 클릭한 경우라면 (UI가 클릭되었는지 확인)
+            if (EventSystem.current.IsPointerOverGameObject())
             {
-                popupPanel.SetActive(true);  // 팝업 창 활성화
+                return;  // UI를 클릭한 경우는 아무 작업도 하지 않음
+            }
+
+            // Raycast로 클릭한 오브젝트 감지
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // 만약 타워 오브젝트를 클릭했다면
+                if (hit.collider.CompareTag("tower"))
+                {
+                    // 팝업 패널 활성화
+                    if (popupPanel != null)
+                    {
+                        popupPanel.SetActive(true);
+                    }
+                }
+                // 다른 오브젝트를 클릭했다면 팝업 패널 비활성화
+                else
+                {
+                    if (popupPanel != null && popupPanel.activeSelf)
+                    {
+                        popupPanel.SetActive(false);
+                    }
+                }
+            }
+            // 아무것도 클릭하지 않은 경우 (빈 공간 클릭 시)
+            else
+            {
+                if (popupPanel != null && popupPanel.activeSelf)
+                {
+                    popupPanel.SetActive(false);
+                }
             }
         }
     }

22f07b793ceebfa57f1dcd5617ffd899042b194f
박서진 <kevin8193@naver.com>
2024-11-24 17:27:00 +0900

    [FEAT] unit, enemy list update

--- 25212120_proj1/Assets/psj/Scripts/gameManager.cs
+++ 25212120_proj1/Assets/psj/Scripts/gameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -14,6 +15,65 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public List<GameObject> enemies = new List<GameObject>();
+    public List<GameObject> units = new List<GameObject>();
+
+    public void AddEnemy(GameObject enemy)
+    {
+        if (!enemies.Contains(enemy))
+        {
+            enemies.Add(enemy);
+        }
+    }
+
+    public void RemoveEnemy(GameObject enemy)
+    {
+        if (enemies.Contains(enemy))
+        {
+            enemies.Remove(enemy);
+        }
+    }
+
+    public void AddUnit(GameObject unit)
+    {
+        if (!units.Contains(unit))
+        {
+            units.Add(unit);
+        }
+    }
+
+    public void RemoveUnit(GameObject unit)
+    {
+        if (units.Contains(unit))
+        {
+            units.Remove(unit);
+        }
+    }
+
+    public void UpgradeAllUnitStat()
+    {
+        foreach (GameObject unit in units)
+        {
+            BaseUnit baseUnit = unit.GetComponent<BaseUnit>();
+            if (baseUnit != null)
+            {
+                baseUnit.Upgrade();
+            }
+        }
+    }
+
+    public void UpgradeAllEnemyStat()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            BaseMonster baseEnemy = enemy.GetComponent<BaseMonster>();
+            if (baseEnemy != null)
+            {
+                baseEnemy.PerformUpgrade();
+            }
         }
     }
 }
